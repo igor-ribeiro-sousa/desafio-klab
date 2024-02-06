f95732acf7ea5a1f2e05eedde746c092239e5e8f
Igor Ribeiro <igorribeirocons@gmail.com>
2024-02-06 20:43:22 -0300

    Primeiro commit: Configuração inicial do projeto Spring

---
+++ src/main/java/com/desafio/klab/KlabApplication.java
@@ -0,0 +1,13 @@
+package com.desafio.klab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KlabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KlabApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/desafio/klab/KlabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desafio.klab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KlabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
