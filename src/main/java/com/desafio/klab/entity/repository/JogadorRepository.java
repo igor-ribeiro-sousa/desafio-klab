85be2c7a699433b6d305e44640b09e9b100df85e
Igor Ribeiro <igorribeirocons@gmail.com>
2024-02-06 21:41:27 -0300

    Implementação dos repositórios para persistência de dados

---
+++ src/main/java/com/desafio/klab/entity/repository/JogadorRepository.java
@@ -0,0 +1,10 @@
+package com.desafio.klab.entity.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.desafio.klab.entity.Jogador;
+
+public interface JogadorRepository extends JpaRepository<Jogador, Long>
+{
+
+}
---
+++ src/main/java/com/desafio/klab/entity/repository/JogoRepository.java
@@ -0,0 +1,10 @@
+package com.desafio.klab.entity.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.desafio.klab.entity.Jogo;
+
+public interface JogoRepository extends JpaRepository<Jogo, Long>
+{
+
+}
