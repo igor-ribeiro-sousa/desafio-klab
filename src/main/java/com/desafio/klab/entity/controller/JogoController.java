fa579e4d255054a01952bac3dceb48d6d6cd3a54
Igor Ribeiro <igorribeirocons@gmail.com>
2024-02-07 23:22:31 -0300

    Adiciona Swagger para documentação da API
    Implementação do Swagger para fornecer uma documentação clara e
    interativa da API. A integração do Swagger permite que os
    desenvolvedores visualizem e testem os endpoints da API de forma fácil e
    eficiente.
    
    - Integração do Swagger para geração automática da documentação da API.
    - Adição de anotações Swagger nos controladores para descrever os
    endpoints, parâmetros e respostas.
    - Configuração do Swagger UI para uma experiência de documentação
    interativa.
    - Atualização da coleção do Postman com os endpoints da API para
    facilitar o teste e a integração.

--- src/main/java/com/desafio/klab/entity/controller/JogoController.java
+++ src/main/java/com/desafio/klab/entity/controller/JogoController.java
@@ -15,14 +15,21 @@ import com.desafio.klab.entity.Jogo;
 import com.desafio.klab.entity.DTO.JogoDTO;
 import com.desafio.klab.entity.service.JogoService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
 @RestController
 @RequestMapping("/jogo")
+@Tag(name = "Desafio Técnico Backend PL KLAB", description = "Api Desafio Cartas.")
 public class JogoController
 {
 
    @Autowired
    private JogoService jogoService;
 
+   @Operation(summary = "Iniciar um novo jogo")
    @PostMapping("/iniciar")
    public ResponseEntity<JogoDTO> iniciar()
    {
@@ -31,6 +38,11 @@ public class JogoController
       return ResponseEntity.ok().body(jogoDTO);
    }
 
+   @Operation(summary = "Buscar um jogo pelo ID")
+   @ApiResponses(value = {
+       @ApiResponse(responseCode = "200", description = "Jogo encontrado"),
+       @ApiResponse(responseCode = "404", description = "Jogo não encontrado")
+   })
    @GetMapping("/{id}")
    public ResponseEntity<JogoDTO> buscarPorId(@PathVariable Long id)
    {
@@ -39,6 +51,7 @@ public class JogoController
       return ResponseEntity.ok().body(jogoDTO);
    }
 
+   @Operation(summary = "Buscar todos os jogos")
    @GetMapping
    public ResponseEntity<List<JogoDTO>> buscarTodos()
    {
